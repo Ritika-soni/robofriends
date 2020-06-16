b45153281d8c7e0fcb13e7a9bc3259877c3c41f6
Ritika-soni <ritikasoni73@gmail.com>
2020-06-16 01:31:34 -0700

    fixing api issues

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
+import { robots } from '../robots';
 import './App.css';
 
 class App extends Component {
@@ -28,17 +30,23 @@ class App extends Component {
     const filteredRobots = robots.filter(robot =>{
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
-    return !robots.length ?
-      <h1>Loading</h1> :
-      (
+    if (robots.length === 0) {
+        return (
+            <CardList robots ={robots} />
+          );
+    } else {
+      return (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
           <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
+            <ErrorBoundary>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundary>
+          </Scroll>-
         </div>
       );
+    }
   }
 }
 
